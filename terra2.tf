137660edba9511a438827ec3e2033d41a41457b2
shyama <skalandoor@paloaltonetworks.com>
2023-02-11 07:44:59 -0800

    2

--- terra2.tf
+++ terra2.tf
@@ -4,5 +4,5 @@ resource "aws_s3_bucket_public_access_block" "example" {
   block_public_acls       = false
   block_public_policy     = true
   ignore_public_acls      = true
-  restrict_public_buckets = true
+  restrict_public_buckets = false
 }
